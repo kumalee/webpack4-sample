1846c38ced9a431bb955bfd420d17cb21e5e1f19
Kuma Li <li.jun.kuma@gmail.com>
2018-02-27 12:35:48 +0800

    Sample 3 - multi entry and pages example by using html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    head: './src/head.js', 
+    index: './src/index.js', 
+    a: './src/a/index.js', 
+    b: './src/b/index.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'page a',
+      inject: true,
+      template: 'public/index.html',
+      filename: 'page-a.html',
+      chunks: ['head', 'index', 'a']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'page b',
+      inject: true,
+      template: 'public/index.html',
+      filename: 'page-b.html',
+      chunks: ['head', 'index', 'b']
+    }),
+  ]
 };
