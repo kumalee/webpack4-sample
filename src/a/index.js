6553be786ab39c2018de271b56c40cd3144c552a
Kuma Li <li.jun.kuma@gmail.com>
2018-02-28 14:36:25 +0800

    Sample 6 - use webpack-dev-server, hot module reload

--- src/a/index.js
+++ src/a/index.js
@@ -6,6 +6,8 @@ import './index.css';
 ReactDOM.render(
     <div>
         { (_.concat(['Hello', 'webpack'], [' 4'])).join(' ') }
+        <p>Updated!</p>
+        <p>Updated 2!</p>
     </div>,
     document.getElementById('root')
 );
